83114c6dd82c3f2d185296ad0696876899b3cd21
MarianellaGL <marianella.gomezluna@gmail.com>
2019-12-20 17:04:25 -0300

    Cambié el location Cinema con un Hashmap con el entity Geolocation

--- CineApi/cine/src/main/java/ar/com/api/cine/entities/Cinema.java
+++ CineApi/cine/src/main/java/ar/com/api/cine/entities/Cinema.java
@@ -3,16 +3,19 @@ package ar.com.api.cine.entities;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import ar.com.api.cine.maps.java.Geocoding;
+
 import java.util.*;
 
 @Document(collection = "Cines")
 public class Cinema {
     @Id
     private ObjectId _id;
-    private String locationCinema;
+    public Map<Integer, Geocoding> locationCinema = new HashMap<Integer, Geocoding>();
+    public String nameCinema;
     private String cinemaCuit;
-    public Map<String, Movie> moviesOn = new HashMap <String, Movie>();
-    
+    public Map<String, Movie> moviesOn = new HashMap<String, Movie>();
 
     public ObjectId get_id() {
         return _id;
@@ -20,14 +23,6 @@ public class Cinema {
 
     public void set_id(ObjectId _id) {
         this._id = _id;
-    }
-
-    public String getLocationCinema() {
-        return locationCinema;
-    }
-
-    public void setLocationCinema(String locationCinema) {
-        this.locationCinema = locationCinema;
     }
 
     public String getCinemaCuit() {
@@ -40,10 +35,29 @@ public class Cinema {
 
     public Cinema() {
     }
-    
-   
 
+    public Map<Integer, Geocoding> getLocationCinema() {
+        return locationCinema;
+    }
 
-    
-    
+    public void setLocationCinema(Map<Integer, Geocoding> locationCinema) {
+        this.locationCinema = locationCinema;
+    }
+
+    public Map<String, Movie> getMoviesOn() {
+        return moviesOn;
+    }
+
+    public void setMoviesOn(Map<String, Movie> moviesOn) {
+        this.moviesOn = moviesOn;
+    }
+
+    public String getNameCinema() {
+        return nameCinema;
+    }
+
+    public void setNameCinema(String nameCinema) {
+        this.nameCinema = nameCinema;
+    }
+
 }
